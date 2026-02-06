4b565f31645963b75ce8f777b69235e3e17e1b1b
Donald Gifford <dgifford@pm.me>
2026-02-06 17:10:29 -0500

    finish

---
+++ cmd/repo-guardian/main.go
@@ -0,0 +1,4 @@
+// Package repoguardian is the entrypoint to running the app. It can be ran
+// manually as a cli application or set to run as a service in a docker
+// container or something similar.
+package repoguardian
